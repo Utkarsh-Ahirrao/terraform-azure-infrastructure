a5c373fd6c6db865f5696e14804430cd82d41f3c
Ut <naruto.nu822042@gmail.com>
2026-08-27 22:53:10 +0530

    Format Terraform files

--- ParentModule/Prod/provider.tf
+++ ParentModule/Prod/provider.tf
@@ -1,16 +1,16 @@
-terraform{
-    required_providers {
-      azurerm = {
-        source = "hashicorp/azurerm"
-        version ="5.0.0"
-      }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.0.0"
     }
-    backend "azurerm" {
-      resource_group_name  = "prod_rg_statefile"
-      storage_account_name = "prodstg2026"  
-      container_name       = "statecontainer"
-      key                  = "prod/terraform.tfstate"
-    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "prod_rg_statefile"
+    storage_account_name = "prodstg2026"
+    container_name       = "statecontainer"
+    key                  = "prod/terraform.tfstate"
+  }
 }
 provider "azurerm" {
   features {}
